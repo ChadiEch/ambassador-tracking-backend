4ec4e3a52fcaacd41f20ab5aeb8f0f4775f0f924
ChadiEch <chadiech18@gmail.com>
2025-07-03 12:45:19 +0300

    asd

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,17 @@ if (!globalThis.crypto) globalThis.crypto = _crypto;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-app.enableCors({
-  origin: [
-    'https://ambassador-dashboard-production.up.railway.app',
-    'http://localhost:3000'
-  ],
-  credentials: true, // Only if you use cookies
-});
-
+  app.enableCors({
+    origin: [
+      'https://ambassador-dashboard-production.up.railway.app',
+      'http://localhost:3000'
+    ],
+    credentials: true,
+  });
 
   const port = process.env.PORT || 5000;
   await app.listen(port);
-  console.log('Server running on port', port);
+  console.log('CORS enabled. Listening on port', port);
 }
 
 bootstrap();
