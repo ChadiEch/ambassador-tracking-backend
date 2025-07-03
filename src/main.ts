7e9438f50fbeaa1f1a8c623dadda6887ccf69761
ChadiEch <chadiech18@gmail.com>
2025-07-03 09:49:02 +0300

    changing localhost

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['https://ambassador-dashboard-production.up.railway.app:8080', 'http://localhost:3000'],
     credentials: true,
   });
+const port = process.env.PORT || 5000;
+await app.listen(port);
 
-  await app.listen(5000); // ✅ set port here
 }
 
 bootstrap();
