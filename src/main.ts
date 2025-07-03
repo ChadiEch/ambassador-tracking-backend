63011c71ed480bf30c06c278eb2db643cee12a10
ChadiEch <chadiech18@gmail.com>
2025-07-03 12:07:43 +0300

    8

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// If you use polyfill, keep it above
 import { webcrypto as _crypto } from 'crypto';
-
-// Ignore TypeScript error, assign for runtime
 // @ts-ignore
-// polyfill-crypto.js
-if (!globalThis.crypto) {
-  globalThis.crypto = require('crypto').webcrypto;
-}
-
+if (!globalThis.crypto) globalThis.crypto = _crypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: [
-      'https://ambassador-dashboard-production.up.railway.app',
-      'http://localhost:3000',
+      'https://ambassador-dashboard-production.up.railway.app', // Frontend (prod)
+      'http://localhost:3000', // Frontend (dev)
     ],
-    credentials: true,
+    credentials: true, // only if your requests use cookies or authorization headers
   });
 
   const port = process.env.PORT || 5000;
   await app.listen(port);
-  console.log('DATABASE_URL:', process.env.DATABASE_URL);
-  console.log('DATABASE:', process.env.database);
+  console.log('Listening on port', port);
 }
-
 bootstrap();
