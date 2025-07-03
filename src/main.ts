414192adba91fcde3695c30eb298f94e134e1793
ChadiEch <chadiech18@gmail.com>
2025-07-03 10:48:54 +0300

    main.ts edit

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { webcrypto as _crypto } from 'crypto';
+
+// Ignore TypeScript error, assign for runtime
+// @ts-ignore
+if (!globalThis.crypto) globalThis.crypto = _crypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://ambassador-dashboard-production.up.railway.app:8080', 'http://localhost:3000'],
+    origin: [
+      'https://ambassador-dashboard-production.up.railway.app:8080',
+      'http://localhost:3000',
+    ],
     credentials: true,
   });
-const port = process.env.PORT || 5000;
-await app.listen(port);
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
-console.log('DATABASE:', process.env.database);
 
+  const port = process.env.PORT || 5000;
+  await app.listen(port);
+  console.log('DATABASE_URL:', process.env.DATABASE_URL);
+  console.log('DATABASE:', process.env.database);
 }
 
 bootstrap();
