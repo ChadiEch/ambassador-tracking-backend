fe30ce853421a0496c8d9f3c32ddf7218ab1e3b1
ChadiEch <chadiech18@gmail.com>
2025-07-03 12:27:25 +0300

    asd

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'https://ambassador-dashboard-production.up.railway.app/',
-      'http://localhost:3000'
+      'https://ambassador-dashboard-production.up.railway.app',  // Your frontend URL
+      'http://localhost:3000', // For local dev
     ],
-    credentials: true,
+    credentials: true, // Only if using cookies/auth headers!
   });
 
   const port = process.env.PORT || 5000;
   await app.listen(port);
-  console.log('Listening on port', port);
+  console.log('Server running on port', port);
 }
 
 bootstrap();
