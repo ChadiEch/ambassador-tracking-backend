d03c6187c38d301c2f54d38c52f301cb6e2f1f3f
ChadiEch <chadiech18@gmail.com>
2025-07-08 12:25:48 +0300

    insta module

--- src/instagram-webhook/instagram-webhook.module.ts
+++ src/instagram-webhook/instagram-webhook.module.ts
@@ -1,6 +1,4 @@
-// src/instagram-webhook/instagram-webhook.module.ts
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { InstagramWebhookController } from './instagram-webhook.controller';
 import { AmbassadorActivity } from '../entities/ambassador-activity.entity';
@@ -8,14 +6,8 @@ import { User } from '../users/entities/user.entity';
 import { InstagramMessage } from '../entities/instagram-message.entity';
 
 @Module({
-  imports: [
-    HttpModule,
-    TypeOrmModule.forFeature([
-      AmbassadorActivity, // ✅ Already imported
-      User ,
-      InstagramMessage,               // ✅ This is the key missing part!
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([AmbassadorActivity, User, InstagramMessage])],
   controllers: [InstagramWebhookController],
+  providers: [],
 })
 export class InstagramWebhookModule {}
