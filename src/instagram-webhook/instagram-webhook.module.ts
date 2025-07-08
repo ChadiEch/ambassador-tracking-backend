21f0bebf34277136dac11330743023fd5e6ff3dc
ChadiEch <chadiech18@gmail.com>
2025-07-08 12:28:30 +0300

    modukle

--- src/instagram-webhook/instagram-webhook.module.ts
+++ src/instagram-webhook/instagram-webhook.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';   // <-- THIS IS CRITICAL
 import { InstagramWebhookController } from './instagram-webhook.controller';
 import { AmbassadorActivity } from '../entities/ambassador-activity.entity';
 import { User } from '../users/entities/user.entity';
 import { InstagramMessage } from '../entities/instagram-message.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AmbassadorActivity, User, InstagramMessage])],
+  imports: [
+    HttpModule,  // <-- Make sure this is present!
+    TypeOrmModule.forFeature([AmbassadorActivity, User, InstagramMessage]),
+  ],
   controllers: [InstagramWebhookController],
   providers: [],
 })
