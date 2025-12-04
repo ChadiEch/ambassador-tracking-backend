5383270d0c01e35678a0e718d8ac683af9015b95
ChadiEch <chadiech18@gmail.com>
2025-12-04 15:39:24 +0200

    12/04/2025

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,7 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+ 
   @Post('login')
   async login(@Body() body: { username: string; password: string }) {
     return this.authService.validateLogin(body.username, body.password);
